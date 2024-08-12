99b7a813264211c8ea18775f3410a62c30860f5b
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2024-08-12 11:55:43 +0200

    Button for watering log deletion

--- app/controllers/watering_logs_controller.rb
+++ app/controllers/watering_logs_controller.rb
@@ -1,6 +1,7 @@
 class WateringLogsController < ApplicationController
+  before_action :set_plant
+
   def create
-    @plant = Plant.find(params[:plant_id])
     if @plant.water(watering_log_params[:created_at])
       redirect_to @plant, notice: "Utworzono watering log"
     else
@@ -8,7 +9,17 @@ class WateringLogsController < ApplicationController
     end
   end
 
+  def destroy
+    @watering_log = WateringLog.find(params[:id])
+    @watering_log.destroy
+    redirect_to plant_path(@plant), notice: "Log deleted"
+  end
+
   private
+
+  def set_plant
+    @plant = Plant.find(params[:plant_id])
+  end
 
   def watering_log_params
     params.require(:watering_log).permit(:watered_at, :notes, :created_at)
